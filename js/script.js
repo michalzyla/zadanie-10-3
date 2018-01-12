caa8e493842e54ad00d1568cb29cc66497fbe995
Michał Żyła <michalzyla7@gmail.com>
2018-01-12 12:54:18 +0100

    Init project

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(function(){
+    console.log('DOM loaded');
+    var carouselList = $("#carousel ul");
+    setInterval(changeSlide, 3000);
+        function changeSlide () {
+            carouselList.animate({'marginLeft':-400}, 600, moveFirstSlide);
+                function moveFirstSlide () {
+                    var firstItem = carouselList.find("li:first");
+                    var lastItem = carouselList.find("li:last");
+                    lastItem.after(firstItem);
+                    carouselList.css({marginLeft:0});
+                };
+        };
+});
